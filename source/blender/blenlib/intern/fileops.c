blender/blender

commit 322c03f13cb5a756902f310c2a34cf78825fe02d

Move callbacks API from BLI to BKE
Preparing for the bigger changes which will be related on passing
dependency graph to various callbacks which need it.

Differential Revision: https://developer.blender.org/D5725

source/blender/blenlib/intern/fileops.c
@@ -35,8 +35,8 @@
 #ifdef WIN32
 #  include <io.h>
 #  include "BLI_winstuff.h"
-#  include "BLI_callbacks.h"
 #  include "BLI_fileops_types.h"
+#  include "BKE_callbacks.h"
 #  include "utf_winfunc.h"
 #  include "utfconv.h"
 #else

source/blender/blenkernel/BKE_callbacks.h
@@ -15,11 +15,11 @@
  */
 
 /** \file
- * \ingroup bli
+ * \ingroup bke
  */
 
-#ifndef __BLI_CALLBACKS_H__
-#define __BLI_CALLBACKS_H__
+#ifndef __BKE_CALLBACKS_H__
+#define __BKE_CALLBACKS_H__
 
 struct ID;
 struct Main;
@@ -32,29 +32,29 @@ struct Main;
  *   For handling jobs (which may in turn cause other handlers to be called).
  */
 typedef enum {
-  BLI_CB_EVT_FRAME_CHANGE_PRE,
-  BLI_CB_EVT_FRAME_CHANGE_POST,
-  BLI_CB_EVT_RENDER_PRE,
-  BLI_CB_EVT_RENDER_POST,
-  BLI_CB_EVT_RENDER_WRITE,
-  BLI_CB_EVT_RENDER_STATS,
-  BLI_CB_EVT_RENDER_INIT,
-  BLI_CB_EVT_RENDER_COMPLETE,
-  BLI_CB_EVT_RENDER_CANCEL,
-  BLI_CB_EVT_LOAD_PRE,
-  BLI_CB_EVT_LOAD_POST,
-  BLI_CB_EVT_SAVE_PRE,
-  BLI_CB_EVT_SAVE_POST,
-  BLI_CB_EVT_UNDO_PRE,
-  BLI_CB_EVT_UNDO_POST,
-  BLI_CB_EVT_REDO_PRE,
-  BLI_CB_EVT_REDO_POST,
-  BLI_CB_EVT_DEPSGRAPH_UPDATE_PRE,
-  BLI_CB_EVT_DEPSGRAPH_UPDATE_POST,
-  BLI_CB_EVT_VERSION_UPDATE,
-  BLI_CB_EVT_LOAD_FACTORY_USERDEF_POST,
-  BLI_CB_EVT_LOAD_FACTORY_STARTUP_POST,
-  BLI_CB_EVT_TOT,
+  BKE_CB_EVT_FRAME_CHANGE_PRE,
+  BKE_CB_EVT_FRAME_CHANGE_POST,
+  BKE_CB_EVT_RENDER_PRE,
+  BKE_CB_EVT_RENDER_POST,
+  BKE_CB_EVT_RENDER_WRITE,
+  BKE_CB_EVT_RENDER_STATS,
+  BKE_CB_EVT_RENDER_INIT,
+  BKE_CB_EVT_RENDER_COMPLETE,
+  BKE_CB_EVT_RENDER_CANCEL,
+  BKE_CB_EVT_LOAD_PRE,
+  BKE_CB_EVT_LOAD_POST,
+  BKE_CB_EVT_SAVE_PRE,
+  BKE_CB_EVT_SAVE_POST,
+  BKE_CB_EVT_UNDO_PRE,
+  BKE_CB_EVT_UNDO_POST,
+  BKE_CB_EVT_REDO_PRE,
+  BKE_CB_EVT_REDO_POST,
+  BKE_CB_EVT_DEPSGRAPH_UPDATE_PRE,
+  BKE_CB_EVT_DEPSGRAPH_UPDATE_POST,
+  BKE_CB_EVT_VERSION_UPDATE,
+  BKE_CB_EVT_LOAD_FACTORY_USERDEF_POST,
+  BKE_CB_EVT_LOAD_FACTORY_STARTUP_POST,
+  BKE_CB_EVT_TOT,
 } eCbEvent;
 
 typedef struct bCallbackFuncStore {
@@ -64,10 +64,10 @@ typedef struct bCallbackFuncStore {
   short alloc;
 } bCallbackFuncStore;
 
-void BLI_callback_exec(struct Main *bmain, struct ID *self, eCbEvent evt);
-void BLI_callback_add(bCallbackFuncStore *funcstore, eCbEvent evt);
+void BKE_callback_exec(struct Main *bmain, struct ID *self, eCbEvent evt);
+void BKE_callback_add(bCallbackFuncStore *funcstore, eCbEvent evt);
 
-void BLI_callback_global_init(void);
-void BLI_callback_global_finalize(void);
+void BKE_callback_global_init(void);
+void BKE_callback_global_finalize(void);
 
-#endif /* __BLI_CALLBACKS_H__ */
+#endif /* __BKE_CALLBACKS_H__ */

source/blender/blenkernel/CMakeLists.txt
@@ -83,6 +83,7 @@ set(SRC
   intern/brush.c
   intern/bvhutils.c
   intern/cachefile.c
+  intern/callbacks.c
   intern/camera.c
   intern/cdderivedmesh.c
   intern/cloth.c
@@ -243,6 +244,7 @@ set(SRC
   BKE_brush.h
   BKE_bvhutils.h
   BKE_cachefile.h
+  BKE_callbacks.h
   BKE_camera.h
   BKE_ccg.h
   BKE_cdderivedmesh.h

source/blender/blenkernel/intern/blender.c
@@ -32,7 +32,6 @@
 #include "BLI_string.h"
 #include "BLI_listbase.h"
 #include "BLI_utildefines.h"
-#include "BLI_callbacks.h"
 
 #include "IMB_imbuf.h"
 #include "IMB_moviecache.h"
@@ -44,6 +43,7 @@
 #include "BKE_blendfile.h"
 #include "BKE_brush.h"
 #include "BKE_cachefile.h"
+#include "BKE_callbacks.h"
 #include "BKE_global.h"
 #include "BKE_idprop.h"
 #include "BKE_image.h"
@@ -95,7 +95,7 @@ void BKE_blender_free(void)
   BKE_brush_system_exit();
   RE_texture_rng_exit();
 
-  BLI_callback_global_finalize();
+  BKE_callback_global_finalize();
 
   IMB_moviecache_destruct();
 

source/blender/blenkernel/intern/callbacks.c
@@ -15,18 +15,19 @@
  */
 
 /** \file
- * \ingroup bli
+ * \ingroup bke
  */
 
 #include "BLI_utildefines.h"
 #include "BLI_listbase.h"
-#include "BLI_callbacks.h"
+
+#include "BKE_callbacks.h"
 
 #include "MEM_guardedalloc.h"
 
-static ListBase callback_slots[BLI_CB_EVT_TOT] = {{NULL}};
+static ListBase callback_slots[BKE_CB_EVT_TOT] = {{NULL}};
 
-void BLI_callback_exec(struct Main *bmain, struct ID *self, eCbEvent evt)
+void BKE_callback_exec(struct Main *bmain, struct ID *self, eCbEvent evt)
 {
   ListBase *lb = &callback_slots[evt];
   bCallbackFuncStore *funcstore;
@@ -36,22 +37,22 @@ void BLI_callback_exec(struct Main *bmain, struct ID *self, eCbEvent evt)
   }
 }
 
-void BLI_callback_add(bCallbackFuncStore *funcstore, eCbEvent evt)
+void BKE_callback_add(bCallbackFuncStore *funcstore, eCbEvent evt)
 {
   ListBase *lb = &callback_slots[evt];
   BLI_addtail(lb, funcstore);
 }
 
-void BLI_callback_global_init(void)
+void BKE_callback_global_init(void)
 {
   /* do nothing */
 }
 
 /* call on application exit */
-void BLI_callback_global_finalize(void)
+void BKE_callback_global_finalize(void)
 {
   eCbEvent evt;
-  for (evt = 0; evt < BLI_CB_EVT_TOT; evt++) {
+  for (evt = 0; evt < BKE_CB_EVT_TOT; evt++) {
     ListBase *lb = &callback_slots[evt];
     bCallbackFuncStore *funcstore;
     bCallbackFuncStore *funcstore_next;

source/blender/blenkernel/intern/scene.c
@@ -48,7 +48,7 @@
 #include "BLI_math.h"
 #include "BLI_blenlib.h"
 #include "BLI_utildefines.h"
-#include "BLI_callbacks.h"
+#include "BKE_callbacks.h"
 #include "BLI_string.h"
 #include "BLI_string_utils.h"
 #include "BLI_threads.h"
@@ -1554,7 +1554,7 @@ static void scene_graph_update_tagged(Depsgraph *depsgraph, Main *bmain, bool on
 
   bool run_callbacks = DEG_id_type_any_updated(depsgraph);
   if (run_callbacks) {
-    BLI_callback_exec(bmain, &scene->id, BLI_CB_EVT_DEPSGRAPH_UPDATE_PRE);
+    BKE_callback_exec(bmain, &scene->id, BKE_CB_EVT_DEPSGRAPH_UPDATE_PRE);
   }
 
   for (int pass = 0; pass < 2; pass++) {
@@ -1572,7 +1572,7 @@ static void scene_graph_update_tagged(Depsgraph *depsgraph, Main *bmain, bool on
     BKE_scene_update_sound(depsgraph, bmain);
     /* Notify python about depsgraph update. */
     if (run_callbacks) {
-      BLI_callback_exec(bmain, &scene->id, BLI_CB_EVT_DEPSGRAPH_UPDATE_POST);
+      BKE_callback_exec(bmain, &scene->id, BKE_CB_EVT_DEPSGRAPH_UPDATE_POST);
     }
     /* Inform editors about possible changes. */
     DEG_ids_check_recalc(bmain, depsgraph, scene, view_layer, false);
@@ -1607,7 +1607,7 @@ void BKE_scene_graph_update_for_newframe(Depsgraph *depsgraph, Main *bmain)
   ViewLayer *view_layer = DEG_get_input_view_layer(depsgraph);
 
   /* Keep this first. */
-  BLI_callback_exec(bmain, &scene->id, BLI_CB_EVT_FRAME_CHANGE_PRE);
+  BKE_callback_exec(bmain, &scene->id, BKE_CB_EVT_FRAME_CHANGE_PRE);
 
   for (int pass = 0; pass < 2; pass++) {
     /* Update animated image textures for particles, modifiers, gpu, etc,
@@ -1629,7 +1629,7 @@ void BKE_scene_graph_update_for_newframe(Depsgraph *depsgraph, Main *bmain)
 
     /* Notify editors and python about recalc. */
     if (pass == 0) {
-      BLI_callback_exec(bmain, &scene->id, BLI_CB_EVT_FRAME_CHANGE_POST);
+      BKE_callback_exec(bmain, &scene->id, BKE_CB_EVT_FRAME_CHANGE_POST);
     }
 
     /* Inform editors about possible changes. */

source/blender/blenlib/BLI_timer.h
@@ -50,7 +50,7 @@ void BLI_timer_execute(void);
 
 void BLI_timer_free(void);
 
-/* This function is to be called next to BLI_CB_EVT_LOAD_PRE, to make sure the module
+/* This function is to be called next to BKE_CB_EVT_LOAD_PRE, to make sure the module
  * is properly configured for the new file. */
 void BLI_timer_on_file_load(void);
 

source/blender/blenlib/CMakeLists.txt
@@ -61,7 +61,6 @@ set(SRC
   intern/bitmap_draw_2d.c
   intern/boxpack_2d.c
   intern/buffer.c
-  intern/callbacks.c
   intern/convexhull_2d.c
   intern/delaunay_2d.c
   intern/dynlib.c
@@ -145,7 +144,6 @@ set(SRC
   BLI_blenlib.h
   BLI_boxpack_2d.h
   BLI_buffer.h
-  BLI_callbacks.h
   BLI_compiler_attrs.h
   BLI_compiler_compat.h
   BLI_compiler_typecheck.h

zyxcambridge/blender_llm

commit 2b86de7f64030212fcdb797206e23cbee612749d

Fix compiler error in Windows introduced in 322c03f13cb5a756902f310c2a34cf78825fe02d

source/blender/blenlib/intern/fileops.c
@@ -36,7 +36,6 @@
 #  include <io.h>
 #  include "BLI_winstuff.h"
 #  include "BLI_fileops_types.h"
-#  include "BKE_callbacks.h"
 #  include "utf_winfunc.h"
 #  include "utfconv.h"
 #else