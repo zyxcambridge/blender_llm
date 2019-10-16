blender/blender

commit 9100982e8097a14ba96f90cf40d5591e1447dd2f

Fix Filebrowser Blender ID filter
When the filtering option was disable we should see all the datablock types.

Differential Revision: https://developer.blender.org/D6033

source/blender/makesdna/DNA_ID.h
@@ -697,6 +697,13 @@ enum {
   FILTER_ID_LP = (1u << 31),
 };
 
+#define FILTER_ID_ALL \
+  (FILTER_ID_AC | FILTER_ID_AR | FILTER_ID_BR | FILTER_ID_CA | FILTER_ID_CU | FILTER_ID_GD | \
+   FILTER_ID_LS | FILTER_ID_LT | FILTER_ID_MA | FILTER_ID_MB | FILTER_ID_MC | FILTER_ID_ME | \
+   FILTER_ID_MSK | FILTER_ID_NT | FILTER_ID_OB | FILTER_ID_PA | FILTER_ID_PAL | FILTER_ID_PC | \
+   FILTER_ID_SCE | FILTER_ID_SPK | FILTER_ID_SO | FILTER_ID_TE | FILTER_ID_TXT | FILTER_ID_VF | \
+   FILTER_ID_WO | FILTER_ID_CF | FILTER_ID_WS | FILTER_ID_LP)
+
 /* IMPORTANT: this enum matches the order currently use in set_listbasepointers,
  * keep them in sync! */
 enum {

source/blender/editors/space_file/filelist.c
@@ -831,7 +831,7 @@ void filelist_setfilter_options(FileList *filelist,
   }
   if ((filelist->filter_data.filter != filter) || (filelist->filter_data.filter_id != filter_id)) {
     filelist->filter_data.filter = filter;
-    filelist->filter_data.filter_id = filter_id;
+    filelist->filter_data.filter_id = (filter & FILE_TYPE_BLENDERLIB) ? filter_id : FILTER_ID_ALL;
     update = true;
   }
   if (!STREQ(filelist->filter_data.filter_glob, filter_glob)) {

source/blender/editors/space_file/filesel.c
@@ -247,13 +247,7 @@ short ED_fileselect_set_params(SpaceFile *sfile)
     }
 
     /* For now, always init filterid to 'all true' */
-    params->filter_id = FILTER_ID_AC | FILTER_ID_AR | FILTER_ID_BR | FILTER_ID_CA | FILTER_ID_CU |
-                        FILTER_ID_GD | FILTER_ID_GR | FILTER_ID_IM | FILTER_ID_LA | FILTER_ID_LS |
-                        FILTER_ID_LT | FILTER_ID_MA | FILTER_ID_MB | FILTER_ID_MC | FILTER_ID_ME |
-                        FILTER_ID_MSK | FILTER_ID_NT | FILTER_ID_OB | FILTER_ID_PA |
-                        FILTER_ID_PAL | FILTER_ID_PC | FILTER_ID_SCE | FILTER_ID_SPK |
-                        FILTER_ID_SO | FILTER_ID_TE | FILTER_ID_TXT | FILTER_ID_VF | FILTER_ID_WO |
-                        FILTER_ID_CF | FILTER_ID_WS | FILTER_ID_LP;
+    params->filter_id = FILTER_ID_ALL;
 
     if (U.uiflag & USER_HIDE_DOT) {
       params->flag |= FILE_HIDE_DOT;

zyxcambridge/blender_llm

commit 1cd74b7a9c50057062ba52ca0f3a1bbf45c72cc3

Fix: Some ID-Filters not enabled on Link/Append
Caused by 9100982e8097.

source/blender/makesdna/DNA_ID.h
@@ -699,10 +699,11 @@ enum {
 
 #define FILTER_ID_ALL \
   (FILTER_ID_AC | FILTER_ID_AR | FILTER_ID_BR | FILTER_ID_CA | FILTER_ID_CU | FILTER_ID_GD | \
-   FILTER_ID_LS | FILTER_ID_LT | FILTER_ID_MA | FILTER_ID_MB | FILTER_ID_MC | FILTER_ID_ME | \
-   FILTER_ID_MSK | FILTER_ID_NT | FILTER_ID_OB | FILTER_ID_PA | FILTER_ID_PAL | FILTER_ID_PC | \
-   FILTER_ID_SCE | FILTER_ID_SPK | FILTER_ID_SO | FILTER_ID_TE | FILTER_ID_TXT | FILTER_ID_VF | \
-   FILTER_ID_WO | FILTER_ID_CF | FILTER_ID_WS | FILTER_ID_LP)
+   FILTER_ID_GR | FILTER_ID_IM | FILTER_ID_LA | FILTER_ID_LS | FILTER_ID_LT | FILTER_ID_MA | \
+   FILTER_ID_MB | FILTER_ID_MC | FILTER_ID_ME | FILTER_ID_MSK | FILTER_ID_NT | FILTER_ID_OB | \
+   FILTER_ID_PA | FILTER_ID_PAL | FILTER_ID_PC | FILTER_ID_SCE | FILTER_ID_SPK | FILTER_ID_SO | \
+   FILTER_ID_TE | FILTER_ID_TXT | FILTER_ID_VF | FILTER_ID_WO | FILTER_ID_CF | FILTER_ID_WS | \
+   FILTER_ID_LP)
 
 /* IMPORTANT: this enum matches the order currently use in set_listbasepointers,
  * keep them in sync! */