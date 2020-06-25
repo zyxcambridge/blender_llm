blender/blender

commit d82c3d86155ea3c7831c7b5ef5d07bc8e2d99394

Fix T62961 EEVEE: Viewport refresh when hovering widgets
We now bypass EEVEE's rendering if the TAA accumulation has ended.

source/blender/draw/engines/eevee/eevee_engine.c
@@ -214,6 +214,10 @@ static void eevee_draw_scene(void *vedata)
     loop_len = MAX2(1, scene->eevee.taa_samples);
   }
 
+  if (stl->effects->bypass_drawing) {
+    loop_len = 0;
+  }
+
   while (loop_len--) {
     float clear_col[4] = {0.0f, 0.0f, 0.0f, 0.0f};
     float clear_depth = 1.0f;
@@ -350,6 +354,11 @@ static void eevee_draw_scene(void *vedata)
   }
   else {
     EEVEE_renderpasses_draw(sldata, vedata);
+  }
+
+  if (stl->effects->bypass_drawing) {
+    /* Restore the depth from sample 1. */
+    GPU_framebuffer_blit(fbl->double_buffer_depth_fb, 0, dfbl->default_fb, 0, GPU_DEPTH_BIT);
   }
 
   EEVEE_renderpasses_draw_debug(vedata);

source/blender/draw/engines/eevee/eevee_private.h
@@ -668,6 +668,7 @@ typedef struct EEVEE_EffectsInfo {
   int taa_render_sample;
   int taa_total_sample;
   float taa_alpha;
+  bool bypass_drawing;
   bool prev_drw_support;
   bool prev_is_navigating;
   float prev_drw_persmat[4][4]; /* Used for checking view validity and reprojection. */

source/blender/draw/engines/eevee/eevee_temporal_sampling.c
@@ -213,6 +213,7 @@ int EEVEE_temporal_sampling_init(EEVEE_ViewLayerData *UNUSED(sldata), EEVEE_Data
    * we accumulate the redraw inside the drawing loop in eevee_draw_scene().
    **/
   effects->taa_render_sample = 1;
+  effects->bypass_drawing = false;
 
   EEVEE_temporal_sampling_create_view(vedata);
 
@@ -255,7 +256,7 @@ int EEVEE_temporal_sampling_init(EEVEE_ViewLayerData *UNUSED(sldata), EEVEE_Data
 
     if (((effects->taa_total_sample == 0) ||
          (effects->taa_current_sample < effects->taa_total_sample)) ||
-        DRW_state_is_image_render()) {
+        (!view_is_valid) || DRW_state_is_image_render()) {
       if (view_is_valid) {
         /* Viewport rendering updates the matrices in `eevee_draw_scene` */
         if (!DRW_state_is_image_render()) {
@@ -268,7 +269,7 @@ int EEVEE_temporal_sampling_init(EEVEE_ViewLayerData *UNUSED(sldata), EEVEE_Data
       }
     }
     else {
-      effects->taa_current_sample = 1;
+      effects->bypass_drawing = true;
     }
 
     return repro_flag | EFFECT_TAA | EFFECT_DOUBLE_BUFFER | EFFECT_DEPTH_DOUBLE_BUFFER |

zyxcambridge/blender_llm

commit 34b1eb7190b4f33aa62cb10c9870efc76df377f9

Fix unreported 1 pixel offset when drawing with GPencil
This bug was introduced in d82c3d86155e

Reviewers: @fclem

source/blender/draw/engines/eevee/eevee_engine.c
@@ -367,6 +367,8 @@ static void eevee_draw_scene(void *vedata)
   EEVEE_volumes_free_smoke_textures();
 
   stl->g_data->view_updated = false;
+
+  DRW_view_set_active(NULL);
 }
 
 static void eevee_view_update(void *vedata)