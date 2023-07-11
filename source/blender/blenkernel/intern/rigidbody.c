blender/blender

commit 9c9664c823343efb0f0bc29e2158b128c365d216

Fix #109928: issues adding rigid bodies via just the world collection
Selecting a rigid body world collection is supposed to set up rigid
bodies for all of its mesh members (if they are not rigid bodies
already), expected result would be just the same as if `Object` > `Rigid
Body` > `Add ...` was used.

Doing it via just the world collection had the following issues though:
- calculating mass would crash on such a freshly created rigid body
objects (be1b32e4e49e falsely assumed `ob->rigidbody_object` is always
present on evaluated objects -- without tagging for depsgraph updates
this is not the case though)
- rigid body simulation would not work even on these freshly created
rigid body objects

Now tag bmain relations and object transforms for update to make both of
these work (following code in `BKE_rigidbody_add_object` that is used
when adding these through `Object` > `Rigid Body` > `Add ...`)

Pull Request: https://projects.blender.org/blender/blender/pulls/109961

source/blender/blenkernel/intern/rigidbody.c
@@ -1339,7 +1339,7 @@ RigidBodyCon *BKE_rigidbody_create_constraint(Scene *scene, Object *ob, short ty
   return rbc;
 }
 
-void BKE_rigidbody_objects_collection_validate(Scene *scene, RigidBodyWorld *rbw)
+void BKE_rigidbody_objects_collection_validate(Main *bmain, Scene *scene, RigidBodyWorld *rbw)
 {
   if (rbw->group != NULL) {
     FOREACH_COLLECTION_OBJECT_RECURSIVE_BEGIN (rbw->group, object) {
@@ -1347,8 +1347,10 @@ void BKE_rigidbody_objects_collection_validate(Scene *scene, RigidBodyWorld *rbw
         continue;
       }
       object->rigidbody_object = BKE_rigidbody_create_object(scene, object, RBO_TYPE_ACTIVE);
+      DEG_id_tag_update(&object->id, ID_RECALC_TRANSFORM);
     }
     FOREACH_COLLECTION_OBJECT_RECURSIVE_END;
+    DEG_relations_tag_update(bmain);
   }
 }
 
@@ -2344,7 +2346,7 @@ void BKE_rigidbody_cache_reset(RigidBodyWorld *rbw) {}
 void BKE_rigidbody_rebuild_world(Depsgraph *depsgraph, Scene *scene, float ctime) {}
 void BKE_rigidbody_do_simulation(Depsgraph *depsgraph, Scene *scene, float ctime) {}
 void BKE_rigidbody_objects_collection_validate(Scene *scene, RigidBodyWorld *rbw) {}
-void BKE_rigidbody_constraints_collection_validate(Scene *scene, RigidBodyWorld *rbw) {}
+void BKE_rigidbody_constraints_collection_validate(Main *bmain, Scene *scene, RigidBodyWorld *rbw) {}
 void BKE_rigidbody_main_collection_object_add(Main *bmain, Collection *collection, Object *object)
 {
 }

source/blender/blenkernel/BKE_rigidbody.h
@@ -93,7 +93,9 @@ struct RigidBodyCon *BKE_rigidbody_create_constraint(struct Scene *scene,
 /**
  * Ensure newly set collections' objects all have required data.
  */
-void BKE_rigidbody_objects_collection_validate(struct Scene *scene, struct RigidBodyWorld *rbw);
+void BKE_rigidbody_objects_collection_validate(struct Main *bmain,
+                                               struct Scene *scene,
+                                               struct RigidBodyWorld *rbw);
 void BKE_rigidbody_constraints_collection_validate(struct Scene *scene,
                                                    struct RigidBodyWorld *rbw);
 

source/blender/blenloader/intern/versioning_280.cc
@@ -2742,7 +2742,7 @@ void do_versions_after_linking_280(FileData *fd, Main *bmain)
         continue;
       }
 
-      BKE_rigidbody_objects_collection_validate(scene, rbw);
+      BKE_rigidbody_objects_collection_validate(bmain, scene, rbw);
       BKE_rigidbody_constraints_collection_validate(scene, rbw);
     }
   }

source/blender/makesrna/intern/rna_rigidbody.c
@@ -186,7 +186,7 @@ static void rna_RigidBodyWorld_objects_collection_update(Main *bmain,
                                                          PointerRNA *ptr)
 {
   RigidBodyWorld *rbw = (RigidBodyWorld *)ptr->data;
-  BKE_rigidbody_objects_collection_validate(scene, rbw);
+  BKE_rigidbody_objects_collection_validate(bmain, scene, rbw);
   rna_RigidBodyWorld_reset(bmain, scene, ptr);
 }
 

zyxcambridge/blender_llm

commit 61d2b3a43aa7d93fd82322a40f9bee607516774d

Fix: Build error in rigid body code with bullet turned off
Caused by 9c9664c823343efb0f0bc29e2158b128c365d216

source/blender/blenkernel/intern/rigidbody.c
@@ -2345,8 +2345,8 @@ bool BKE_rigidbody_check_sim_running(RigidBodyWorld *rbw, float ctime)
 void BKE_rigidbody_cache_reset(RigidBodyWorld *rbw) {}
 void BKE_rigidbody_rebuild_world(Depsgraph *depsgraph, Scene *scene, float ctime) {}
 void BKE_rigidbody_do_simulation(Depsgraph *depsgraph, Scene *scene, float ctime) {}
-void BKE_rigidbody_objects_collection_validate(Scene *scene, RigidBodyWorld *rbw) {}
-void BKE_rigidbody_constraints_collection_validate(Main *bmain, Scene *scene, RigidBodyWorld *rbw) {}
+void BKE_rigidbody_objects_collection_validate(Main *bmain, Scene *scene, RigidBodyWorld *rbw) {}
+void BKE_rigidbody_constraints_collection_validate(Scene *scene, RigidBodyWorld *rbw) {}
 void BKE_rigidbody_main_collection_object_add(Main *bmain, Collection *collection, Object *object)
 {
 }