blender/blender

commit 0a633a4e07fd309d8d4d2dbf7b2f99b7d0be3b96

Refactor: Untangle confusing & unsafe outliner tree element data passing
The outliner tree element creation function passed a `void *idv`
argument and did weird and hard to follow stuff with it (like casting it
to `ID *` even though it wouldn't point to an ID, and further overriding
it based on the element type). This change tries to untangle this, so
that it's easy to follow what's going on, and so that a bunch of casts
from non-ID data to ID pointers are removed.

Basically the void pointer is split into an ID pointer for the owning ID
(if any) and a void pointer for custom data for the element creation.
This can be made type safe still, but that's for another commit. It also
allows us to remove some wrapper structs. The element creation function
itself no longer needs to know what kind of data is passed via the void
pointer. So this change has a number of benefits in fact.
Also improves/adds related comments.

Was quite careful to not cause behavioral changes.

source/blender/editors/space_outliner/tree/tree_element_anim_data.cc
@@ -31,9 +31,18 @@ TreeElementAnimData::TreeElementAnimData(TreeElement &legacy_te, AnimData &anim_
 
 void TreeElementAnimData::expand(SpaceOutliner &space_outliner) const
 {
+  if (!anim_data_.action) {
+    return;
+  }
+
   /* Animation data-block itself. */
-  outliner_add_element(
-      &space_outliner, &legacy_te_.subtree, anim_data_.action, &legacy_te_, TSE_SOME_ID, 0);
+  outliner_add_element(&space_outliner,
+                       &legacy_te_.subtree,
+                       reinterpret_cast<ID *>(anim_data_.action),
+                       nullptr,
+                       &legacy_te_,
+                       TSE_SOME_ID,
+                       0);
 
   expand_drivers(space_outliner);
   expand_NLA_tracks(space_outliner);
@@ -45,7 +54,7 @@ void TreeElementAnimData::expand_drivers(SpaceOutliner &space_outliner) const
     return;
   }
   outliner_add_element(
-      &space_outliner, &legacy_te_.subtree, &anim_data_, &legacy_te_, TSE_DRIVER_BASE, 0);
+      &space_outliner, &legacy_te_.subtree, nullptr, &anim_data_, &legacy_te_, TSE_DRIVER_BASE, 0);
 }
 
 void TreeElementAnimData::expand_NLA_tracks(SpaceOutliner &space_outliner) const
@@ -53,7 +62,8 @@ void TreeElementAnimData::expand_NLA_tracks(SpaceOutliner &space_outliner) const
   if (BLI_listbase_is_empty(&anim_data_.nla_tracks)) {
     return;
   }
-  outliner_add_element(&space_outliner, &legacy_te_.subtree, &anim_data_, &legacy_te_, TSE_NLA, 0);
+  outliner_add_element(
+      &space_outliner, &legacy_te_.subtree, nullptr, &anim_data_, &legacy_te_, TSE_NLA, 0);
 }
 
 }  // namespace blender::ed::outliner

source/blender/editors/space_outliner/outliner_intern.hh
@@ -295,56 +295,6 @@ struct IDsSelectedData {
   ListBase selected_array;
 };
 
-struct BoneElementCreateData {
-  ID *armature_id;
-  Bone *bone;
-};
-
-struct EditBoneElementCreateData {
-  ID *armature_id;
-  EditBone *ebone;
-};
-
-struct ConstraintElementCreateData {
-  Object *object;
-  bConstraint *con;
-};
-
-struct DeformGroupElementCreateData {
-  Object *object;
-  bDeformGroup *defgroup;
-};
-
-struct GPencilEffectElementCreateData {
-  Object *object;
-  ShaderFxData *fx;
-};
-
-struct ModifierCreateElementData {
-  Object *object;
-  ModifierDataStoreElem *md;
-};
-
-struct ParticleSystemElementCreateData {
-  Object *object;
-  ParticleSystem *psys;
-};
-
-struct PoseChannelElementCreateData {
-  Object *object;
-  bPoseChannel *pchan;
-};
-
-struct PoseGroupElementCreateData {
-  Object *object;
-  bActionGroup *agrp;
-};
-
-struct ViewLayerElementCreateData {
-  Scene *scene;
-  ViewLayer *view_layer;
-};
-
 TreeTraversalAction outliner_collect_selected_collections(TreeElement *te, void *customdata);
 TreeTraversalAction outliner_collect_selected_objects(TreeElement *te, void *customdata);
 

source/blender/editors/space_outliner/outliner_tree.cc
@@ -210,7 +210,8 @@ static void outliner_add_line_styles(SpaceOutliner *space_outliner,
           continue;
         }
         linestyle->id.tag &= ~LIB_TAG_DOIT;
-        outliner_add_element(space_outliner, lb, linestyle, te, TSE_SOME_ID, 0);
+        outliner_add_element(
+            space_outliner, lb, reinterpret_cast<ID *>(linestyle), nullptr, te, TSE_SOME_ID, 0);
       }
     }
   }
@@ -219,86 +220,46 @@ static void outliner_add_line_styles(SpaceOutliner *space_outliner,
 
 TreeElement *outliner_add_element(SpaceOutliner *space_outliner,
                                   ListBase *lb,
-                                  void *idv,
+                                  ID *owner_id,
+                                  void *create_data,
                                   TreeElement *parent,
                                   short type,
                                   short index,
                                   const bool expand)
 {
-  ID *id = static_cast<ID *>(idv);
+  /* Pointer to store in #TreeStoreElem.id to identify the element over rebuilds and reconstruct it
+   * on file read. */
+  /* FIXME: This is may be an arbitrary void pointer that is cast to an ID pointer. Could be a
+   * temporary stack pointer even. Often works reliably enough at runtime, and file reading handles
+   * cases where data can't be reconstructed just fine (pointer is null`ed). This is still
+   * completely type unsafe and error-prone. */
+  ID *persistent_dataptr = owner_id ? owner_id : static_cast<ID *>(create_data);
 
-  if (ELEM(type, TSE_RNA_STRUCT, TSE_RNA_PROPERTY, TSE_RNA_ARRAY_ELEM)) {
-    id = ((PointerRNA *)idv)->owner_id;
-    if (!id) {
-      id = static_cast<ID *>(((PointerRNA *)idv)->data);
-    }
-  }
-  else if (type == TSE_GP_LAYER) {
-    /* idv is the layer itself */
-    id = TREESTORE(parent)->id;
-  }
-  else if (type == TSE_GREASE_PENCIL_NODE) {
-    /* idv is the layer itself */
-    id = TREESTORE(parent)->id;
-  }
-  else if (ELEM(type, TSE_GENERIC_LABEL)) {
-    id = nullptr;
-  }
-  else if (ELEM(type, TSE_LIBRARY_OVERRIDE, TSE_LIBRARY_OVERRIDE_OPERATION)) {
-    id = &static_cast<TreeElementOverridesData *>(idv)->id;
-  }
-  else if (type == TSE_BONE) {
-    id = static_cast<BoneElementCreateData *>(idv)->armature_id;
-  }
-  else if (type == TSE_EBONE) {
-    id = static_cast<EditBoneElementCreateData *>(idv)->armature_id;
-  }
-  else if (type == TSE_GPENCIL_EFFECT) {
-    id = &static_cast<GPencilEffectElementCreateData *>(idv)->object->id;
-  }
-  else if (type == TSE_DEFGROUP) {
-    id = &static_cast<DeformGroupElementCreateData *>(idv)->object->id;
-  }
-  else if (type == TSE_LINKED_PSYS) {
-    id = &static_cast<ParticleSystemElementCreateData *>(idv)->object->id;
-  }
-  else if (type == TSE_CONSTRAINT) {
-    id = &static_cast<ConstraintElementCreateData *>(idv)->object->id;
-  }
-  else if (type == TSE_POSEGRP) {
-    id = &static_cast<PoseGroupElementCreateData *>(idv)->object->id;
-  }
-  else if (type == TSE_R_LAYER) {
-    id = &static_cast<ViewLayerElementCreateData *>(idv)->scene->id;
-  }
-  else if (type == TSE_POSE_CHANNEL) {
-    id = &static_cast<PoseChannelElementCreateData *>(idv)->object->id;
-  }
-  else if (type == TSE_LAYER_COLLECTION) {
-    id = &static_cast<LayerCollection *>(idv)->collection->id;
-  }
-  else if (type == TSE_MODIFIER) {
-    id = &static_cast<ModifierCreateElementData *>(idv)->object->id;
+  if ((owner_id == nullptr) && ELEM(type, TSE_RNA_STRUCT, TSE_RNA_PROPERTY, TSE_RNA_ARRAY_ELEM)) {
+    persistent_dataptr = static_cast<ID *>(((PointerRNA *)create_data)->data);
   }
 
   /* exceptions */
-  if (ELEM(type, TSE_ID_BASE, TSE_GENERIC_LABEL)) {
+  if (ELEM(type, TSE_ID_BASE)) {
     /* pass */
   }
-  else if (id == nullptr) {
+  else if (ELEM(type, TSE_GENERIC_LABEL)) {
+    persistent_dataptr = nullptr;
+  }
+  else if (persistent_dataptr == nullptr) {
     return nullptr;
   }
 
   if (type == TSE_SOME_ID) {
     /* Real ID, ensure we do not get non-outliner ID types here... */
-    BLI_assert(TREESTORE_ID_TYPE(id));
+    BLI_assert(TREESTORE_ID_TYPE(owner_id));
   }
 
   TreeElement *te = MEM_new<TreeElement>(__func__);
   /* add to the visual tree */
   BLI_addtail(lb, te);
   /* add to the storage */
-  check_persistent(space_outliner, te, id, type, index);
+  check_persistent(space_outliner, te, persistent_dataptr, type, index);
   TreeStoreElem *tselem = TREESTORE(te);
 
   /* if we are searching for something expand to see child elements */
@@ -311,7 +272,7 @@ TreeElement *outliner_add_element(SpaceOutliner *space_outliner,
 
   /* New inheritance based element representation. Not all element types support this yet,
    * eventually it should replace #TreeElement entirely. */
-  te->abstract_element = AbstractTreeElement::create_from_type(type, *te, idv);
+  te->abstract_element = AbstractTreeElement::create_from_type(type, *te, owner_id, create_data);
   if (te->abstract_element) {
     /* Element types ported to the new design are expected to have their name set at this point! */
     BLI_assert(te->name != nullptr);
@@ -386,7 +347,7 @@ TreeElement *outliner_add_element(SpaceOutliner *space_outliner,
 
     /* The new type design sets the name already, don't override that here. We need to figure out
      * how to deal with the idcode for non-TSE_SOME_ID types still. Some rely on it... */
-    te->idcode = GS(id->name);
+    te->idcode = GS(owner_id->name);
   }
 
   if (!expand) {
@@ -417,7 +378,8 @@ BLI_INLINE void outliner_add_collection_objects(SpaceOutliner *space_outliner,
                                                 TreeElement *parent)
 {
   LISTBASE_FOREACH (CollectionObject *, cob, &collection->gobject) {
-    outliner_add_element(space_outliner, tree, cob->ob, parent, TSE_SOME_ID, 0);
+    outliner_add_element(
+        space_outliner, tree, reinterpret_cast<ID *>(cob->ob), nullptr, parent, TSE_SOME_ID, 0);
   }
 }
 
@@ -429,7 +391,7 @@ TreeElement *outliner_add_collection_recursive(SpaceOutliner *space_outliner,
 
   LISTBASE_FOREACH (CollectionChild *, child, &collection->children) {
     outliner_add_element(
-        space_outliner, &ten->subtree, &child->collection->id, ten, TSE_SOME_ID, 0);
+        space_outliner, &ten->subtree, &child->collection->id, nullptr, ten, TSE_SOME_ID, 0);
   }
 
   if (space_outliner->outlinevis != SO_SCENES) {

source/blender/editors/space_outliner/tree/tree_display_data.cc
@@ -32,7 +32,7 @@ ListBase TreeDisplayDataAPI::build_tree(const TreeSourceData &source_data)
   RNA_main_pointer_create(source_data.bmain, &mainptr);
 
   TreeElement *te = outliner_add_element(
-      &space_outliner_, &tree, (void *)&mainptr, nullptr, TSE_RNA_STRUCT, -1);
+      &space_outliner_, &tree, nullptr, (void *)&mainptr, nullptr, TSE_RNA_STRUCT, -1);
 
   /* On first view open parent data elements */
   const int show_opened = !space_outliner_.treestore ||

source/blender/editors/space_outliner/tree/tree_display_libraries.cc
@@ -137,10 +137,17 @@ TreeElement *TreeDisplayLibraries::add_library_contents(Main &mainvar, ListBase 
       if (!tenlib) {
         /* Create library tree element on demand, depending if there are any data-blocks. */
         if (lib) {
-          tenlib = outliner_add_element(&space_outliner_, &lb, lib, nullptr, TSE_SOME_ID, 0);
+          tenlib = outliner_add_element(&space_outliner_,
+                                        &lb,
+                                        reinterpret_cast<ID *>(lib),
+                                        nullptr,
+                                        nullptr,
+                                        TSE_SOME_ID,
+                                        0);
         }
         else {
-          tenlib = outliner_add_element(&space_outliner_, &lb, &mainvar, nullptr, TSE_ID_BASE, 0);
+          tenlib = outliner_add_element(
+              &space_outliner_, &lb, nullptr, &mainvar, nullptr, TSE_ID_BASE, 0);
           tenlib->name = IFACE_("Current File");
         }
       }
@@ -153,15 +160,21 @@ TreeElement *TreeDisplayLibraries::add_library_contents(Main &mainvar, ListBase 
           ten = tenlib;
         }
         else {
-          ten = outliner_add_element(
-              &space_outliner_, &tenlib->subtree, lib, nullptr, TSE_ID_BASE, a);
+          ten = outliner_add_element(&space_outliner_,
+                                     &tenlib->subtree,
+                                     reinterpret_cast<ID *>(lib),
+                                     nullptr,
+                                     nullptr,
+                                     TSE_ID_BASE,
+                                     a);
           ten->directdata = lbarray[a];
           ten->name = outliner_idcode_to_plural(GS(id->name));
         }
 
         for (ID *id : List<ID>(lbarray[a])) {
           if (library_id_filter_poll(lib, id)) {
-            outliner_add_element(&space_outliner_, &ten->subtree, id, ten, TSE_SOME_ID, 0);
+            outliner_add_element(
+                &space_outliner_, &ten->subtree, id, nullptr, ten, TSE_SOME_ID, 0);
           }
         }
       }

source/blender/editors/space_outliner/tree/tree_display_orphaned.cc
@@ -58,7 +58,8 @@ ListBase TreeDisplayIDOrphans::build_tree(const TreeSourceData &source_data)
     TreeElement *te = nullptr;
     if (!filter_id_type) {
       ID *id = (ID *)lbarray[a]->first;
-      te = outliner_add_element(&space_outliner_, &tree, lbarray[a], nullptr, TSE_ID_BASE, 0);
+      te = outliner_add_element(
+          &space_outliner_, &tree, nullptr, lbarray[a], nullptr, TSE_ID_BASE, 0);
       te->directdata = lbarray[a];
       te->name = outliner_idcode_to_plural(GS(id->name));
     }
@@ -67,7 +68,7 @@ ListBase TreeDisplayIDOrphans::build_tree(const TreeSourceData &source_data)
     for (ID *id : List<ID>(lbarray[a])) {
       if (ID_REAL_USERS(id) <= 0) {
         outliner_add_element(
-            &space_outliner_, (te) ? &te->subtree : &tree, id, te, TSE_SOME_ID, 0);
+            &space_outliner_, (te) ? &te->subtree : &tree, id, nullptr, te, TSE_SOME_ID, 0);
       }
     }
   }

source/blender/editors/space_outliner/tree/tree_display_override_library_hierarchies.cc
@@ -41,7 +41,7 @@ ListBase TreeDisplayOverrideLibraryHierarchies::build_tree(const TreeSourceData 
 
   /* First step: Build "Current File" hierarchy. */
   TreeElement *current_file_te = outliner_add_element(
-      &space_outliner_, &tree, source_data.bmain, nullptr, TSE_ID_BASE, -1);
+      &space_outliner_, &tree, nullptr, source_data.bmain, nullptr, TSE_ID_BASE, -1);
   current_file_te->name = IFACE_("Current File");
   AbstractTreeElement::uncollapse_by_default(current_file_te);
   {
@@ -49,8 +49,13 @@ ListBase TreeDisplayOverrideLibraryHierarchies::build_tree(const TreeSourceData 
 
     /* Add dummy child if there's nothing to display. */
     if (BLI_listbase_is_empty(&current_file_te->subtree)) {
-      TreeElement *dummy_te = outliner_add_element(
-          &space_outliner_, &current_file_te->subtree, nullptr, current_file_te, TSE_ID_BASE, 0);
+      TreeElement *dummy_te = outliner_add_element(&space_outliner_,
+                                                   &current_file_te->subtree,
+                                                   nullptr,
+                                                   nullptr,
+                                                   current_file_te,
+                                                   TSE_ID_BASE,
+                                                   0);
       dummy_te->name = IFACE_("No Library Overrides");
     }
   }
@@ -60,7 +65,7 @@ ListBase TreeDisplayOverrideLibraryHierarchies::build_tree(const TreeSourceData 
        lib = (Library *)lib->id.next)
   {
     TreeElement *tenlib = outliner_add_element(
-        &space_outliner_, &tree, lib, nullptr, TSE_SOME_ID, 0);
+        &space_outliner_, &tree, reinterpret_cast<ID *>(lib), nullptr, nullptr, TSE_SOME_ID, 0);
     build_hierarchy_for_lib_or_main(source_data.bmain, *tenlib, lib);
   }
 
@@ -146,7 +151,8 @@ ListBase TreeDisplayOverrideLibraryHierarchies::build_hierarchy_for_lib_or_main(
     TreeElement *new_base_te = id_base_te_map.lookup_or_add_cb(GS(iter_id->name), [&]() {
       TreeElement *new_te = outliner_add_element(&space_outliner_,
                                                  &parent_te.subtree,
-                                                 lib ? (void *)lib : bmain,
+                                                 reinterpret_cast<ID *>(lib),
+                                                 bmain,
                                                  &parent_te,
                                                  TSE_ID_BASE,
                                                  base_index++);
@@ -154,8 +160,14 @@ ListBase TreeDisplayOverrideLibraryHierarchies::build_hierarchy_for_lib_or_main(
       return new_te;
     });
 
-    TreeElement *new_id_te = outliner_add_element(
-        &space_outliner_, &new_base_te->subtree, iter_id, new_base_te, TSE_SOME_ID, 0, false);
+    TreeElement *new_id_te = outliner_add_element(&space_outliner_,
+                                                  &new_base_te->subtree,
+                                                  iter_id,
+                                                  nullptr,
+                                                  new_base_te,
+                                                  TSE_SOME_ID,
+                                                  0,
+                                                  false);
 
     builder.build_hierarchy_for_ID(*iter_id, *new_id_te);
   }
@@ -222,8 +234,14 @@ void OverrideIDHierarchyBuilder::build_hierarchy_for_ID_recursive(const ID &pare
       return FOREACH_BREAK;
     }
 
-    TreeElement *new_te = outliner_add_element(
-        &space_outliner_, &te_to_expand.subtree, &id, &te_to_expand, TSE_SOME_ID, 0, false);
+    TreeElement *new_te = outliner_add_element(&space_outliner_,
+                                               &te_to_expand.subtree,
+                                               &id,
+                                               nullptr,
+                                               &te_to_expand,
+                                               TSE_SOME_ID,
+                                               0,
+                                               false);
 
     build_data.sibling_ids.add(&id);
 

source/blender/editors/space_outliner/tree/tree_display_override_library_properties.cc
@@ -88,7 +88,7 @@ ListBase TreeDisplayOverrideLibraryProperties::add_library_contents(Main &mainva
 
     if (!filter_id_type) {
       id_base_te = outliner_add_element(
-          &space_outliner_, &tree, lbarray[a], nullptr, TSE_ID_BASE, 0);
+          &space_outliner_, &tree, nullptr, lbarray[a], nullptr, TSE_ID_BASE, 0);
       id_base_te->directdata = lbarray[a];
       id_base_te->name = outliner_idcode_to_plural(GS(id->name));
 
@@ -98,7 +98,7 @@ ListBase TreeDisplayOverrideLibraryProperties::add_library_contents(Main &mainva
     for (ID *id : List<ID>(lbarray[a])) {
       if (ID_IS_OVERRIDE_LIBRARY_REAL(id) && !ID_IS_LINKED(id)) {
         TreeElement *override_tree_element = outliner_add_element(
-            &space_outliner_, lb_to_expand, id, id_base_te, TSE_LIBRARY_OVERRIDE_BASE, 0);
+            &space_outliner_, lb_to_expand, id, nullptr, id_base_te, TSE_LIBRARY_OVERRIDE_BASE, 0);
 
         if (BLI_listbase_is_empty(&override_tree_element->subtree)) {
           outliner_free_tree_element(override_tree_element, lb_to_expand);

zyxcambridge/blender_llm

commit 2307fb413d3712a0f522a9bd6a02b2ee25154b8b

Fix #118475: Regression: NLA/driver data missing in outliner
Caused by 0a633a4e07
NLA and driver tree-elements were not added to the outliner when
"action" is unlinked from the object. This is due to the wrong `if`
condition preventing the excution of `expand_drivers/expand_NLA_tracks`.

Pull Request: https://projects.blender.org/blender/blender/pulls/118597

source/blender/editors/space_outliner/tree/tree_element_anim_data.cc
@@ -31,17 +31,16 @@ TreeElementAnimData::TreeElementAnimData(TreeElement &legacy_te, AnimData &anim_
 
 void TreeElementAnimData::expand(SpaceOutliner & /*space_outliner*/) const
 {
-  if (!anim_data_.action) {
-    return;
-  }
 
-  /* Animation data-block itself. */
-  add_element(&legacy_te_.subtree,
-              reinterpret_cast<ID *>(anim_data_.action),
-              nullptr,
-              &legacy_te_,
-              TSE_SOME_ID,
-              0);
+  if (anim_data_.action) {
+    /* Animation data-block itself. */
+    add_element(&legacy_te_.subtree,
+                reinterpret_cast<ID *>(anim_data_.action),
+                nullptr,
+                &legacy_te_,
+                TSE_SOME_ID,
+                0);
+  }
 
   expand_drivers();
   expand_NLA_tracks();