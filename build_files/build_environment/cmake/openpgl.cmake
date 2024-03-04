blender/blender

commit 28e771372bcad3194bc55f56e9da32361036b241

Cleanup: wrap lines for in CMake's build-environment for readability
- Commands which have arguments split over multiple lines use
  indented lines.
- Wrap lines where multiple commands run using "&&".
- Blank lines between multiple commands helps the text from becoming
  too dense.

build_files/build_environment/cmake/openpgl.cmake
@@ -24,7 +24,12 @@ ExternalProject_Add(external_openpgl
   DOWNLOAD_DIR ${DOWNLOAD_DIR}
   URL_HASH ${OPENPGL_HASH_TYPE}=${OPENPGL_HASH}
   PREFIX ${BUILD_DIR}/openpgl
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LIBDIR}/openpgl ${DEFAULT_CMAKE_FLAGS} ${OPENPGL_EXTRA_ARGS}
+
+  CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX=${LIBDIR}/openpgl
+    ${DEFAULT_CMAKE_FLAGS}
+    ${OPENPGL_EXTRA_ARGS}
+
   INSTALL_DIR ${LIBDIR}/openpgl
 )
 
@@ -36,13 +41,21 @@ add_dependencies(
 if(WIN32)
   if(BUILD_MODE STREQUAL Release)
     ExternalProject_Add_Step(external_openpgl after_install
-      COMMAND ${CMAKE_COMMAND} -E copy_directory ${LIBDIR}/openpgl ${HARVEST_TARGET}/openpgl
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+        ${LIBDIR}/openpgl
+        ${HARVEST_TARGET}/openpgl
+
       DEPENDEES install
     )
   else()
   ExternalProject_Add_Step(external_openpgl after_install
-      COMMAND ${CMAKE_COMMAND} -E copy ${LIBDIR}/openpgl/lib/openpgl_d.lib ${HARVEST_TARGET}/openpgl/lib/openpgl_d.lib
-      COMMAND ${CMAKE_COMMAND} -E copy ${LIBDIR}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake ${HARVEST_TARGET}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake
+      COMMAND ${CMAKE_COMMAND} -E copy $
+        {LIBDIR}/openpgl/lib/openpgl_d.lib
+        ${HARVEST_TARGET}/openpgl/lib/openpgl_d.lib
+      COMMAND ${CMAKE_COMMAND} -E copy $
+        {LIBDIR}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake
+        ${HARVEST_TARGET}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake
+
       DEPENDEES install
     )
   endif()

zyxcambridge/blender_llm

commit 05371d7306bc93ef43190975be9d267e9c96f54e

deps_builder: Fix openpgl build error on windows
$ was on the wrong line causing a build error,
issue introduced in 28e771372bcad3194bc55f56e9da32361036b241

build_files/build_environment/cmake/openpgl.cmake
@@ -49,11 +49,11 @@ if(WIN32)
     )
   else()
   ExternalProject_Add_Step(external_openpgl after_install
-      COMMAND ${CMAKE_COMMAND} -E copy $
-        {LIBDIR}/openpgl/lib/openpgl_d.lib
+      COMMAND ${CMAKE_COMMAND} -E copy
+        ${LIBDIR}/openpgl/lib/openpgl_d.lib
         ${HARVEST_TARGET}/openpgl/lib/openpgl_d.lib
-      COMMAND ${CMAKE_COMMAND} -E copy $
-        {LIBDIR}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake
+      COMMAND ${CMAKE_COMMAND} -E copy
+        ${LIBDIR}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake
         ${HARVEST_TARGET}/openpgl/lib/cmake/openpgl-${OPENPGL_SHORT_VERSION}/openpgl_Exports-debug.cmake
 
       DEPENDEES install