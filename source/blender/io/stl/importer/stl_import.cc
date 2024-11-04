blender/blender

commit d1455c41389302eadeeae1d291b482c1fa499a9f

Geometry Nodes: Add STL Import Node
This commit adds an initial STL import node, the first of the nodes from the
current Google Summer of Code Project [0]. The importer is refactored to
output a mesh pointer, and a node is added to wrap around the importer.
The node supports error messages from the importer. A new experimental
option is added to hide the nodes by default until they're ready to be exposed
generally.

0: https://devtalk.blender.org/t/gsoc-2024-geometry-nodes-file-import-nodes/34482)

Pull Request: https://projects.blender.org/blender/blender/pulls/122418

source/blender/io/stl/importer/stl_import.cc
@@ -47,18 +47,7 @@ void stl_import_report_error(FILE *file)
   }
 }
 
-void importer_main(const bContext *C, const STLImportParams &import_params)
-{
-  Main *bmain = CTX_data_main(C);
-  Scene *scene = CTX_data_scene(C);
-  ViewLayer *view_layer = CTX_data_view_layer(C);
-  importer_main(bmain, scene, view_layer, import_params);
-}
-
-void importer_main(Main *bmain,
-                   Scene *scene,
-                   ViewLayer *view_layer,
-                   const STLImportParams &import_params)
+Mesh *read_stl_file(const STLImportParams &import_params)
 {
   FILE *file = BLI_fopen(import_params.filepath, "rb");
   if (!file) {
@@ -67,7 +56,7 @@ void importer_main(Main *bmain,
                 RPT_ERROR,
                 "STL Import: Cannot open file '%s'",
                 import_params.filepath);
-    return;
+    return nullptr;
   }
   BLI_SCOPED_DEFER([&]() { fclose(file); });
 
@@ -84,14 +73,9 @@ void importer_main(Main *bmain,
                 RPT_ERROR,
                 "STL Import: Failed to read file '%s'",
                 import_params.filepath);
-    return;
+    return nullptr;
   }
   bool is_ascii_stl = (file_size != (BINARY_HEADER_SIZE + 4 + BINARY_STRIDE * num_tri));
-
-  /* Name used for both mesh and object. */
-  char ob_name[FILE_MAX];
-  STRNCPY(ob_name, BLI_path_basename(import_params.filepath));
-  BLI_path_extension_strip(ob_name);
 
   Mesh *mesh = is_ascii_stl ?
                    read_stl_ascii(import_params.filepath, import_params.use_facet_normal) :
@@ -103,7 +87,7 @@ void importer_main(Main *bmain,
                 RPT_ERROR,
                 "STL Import: Failed to import mesh from file '%s'",
                 import_params.filepath);
-    return;
+    return nullptr;
   }
 
   if (import_params.use_mesh_validate) {
@@ -113,6 +97,29 @@ void importer_main(Main *bmain,
 #endif
     BKE_mesh_validate(mesh, verbose_validate, false);
   }
+
+  return mesh;
+}
+
+void importer_main(const bContext *C, const STLImportParams &import_params)
+{
+  Main *bmain = CTX_data_main(C);
+  Scene *scene = CTX_data_scene(C);
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+  importer_main(bmain, scene, view_layer, import_params);
+}
+
+void importer_main(Main *bmain,
+                   Scene *scene,
+                   ViewLayer *view_layer,
+                   const STLImportParams &import_params)
+{
+  /* Name used for both mesh and object. */
+  char ob_name[FILE_MAX];
+  STRNCPY(ob_name, BLI_path_basename(import_params.filepath));
+  BLI_path_extension_strip(ob_name);
+
+  Mesh *mesh = read_stl_file(import_params);
 
   Mesh *mesh_in_main = BKE_mesh_add(bmain, ob_name);
   BKE_mesh_nomain_to_mesh(mesh, mesh_in_main, nullptr);

scripts/startup/bl_ui/node_add_menu_geometry.py
@@ -243,11 +243,13 @@ class NODE_MT_geometry_node_GEO_INPUT(Menu):
     bl_idname = "NODE_MT_geometry_node_GEO_INPUT"
     bl_label = "Input"
 
-    def draw(self, _context):
+    def draw(self, context):
         layout = self.layout
         layout.menu("NODE_MT_geometry_node_GEO_INPUT_CONSTANT")
         layout.menu("NODE_MT_geometry_node_GEO_INPUT_GROUP")
         layout.menu("NODE_MT_geometry_node_GEO_INPUT_SCENE")
+        if context.preferences.experimental.use_new_file_import_nodes:
+            layout.menu("NODE_MT_category_IMPORT")
         node_add_menu.draw_assets_for_catalog(layout, self.bl_label)
 
 
@@ -448,6 +450,14 @@ class NODE_MT_category_PRIMITIVES_MESH(Menu):
         node_add_menu.add_node_type(layout, "GeometryNodeMeshUVSphere")
         node_add_menu.draw_assets_for_catalog(layout, "Mesh/Primitives")
 
+class NODE_MT_category_IMPORT(Menu):
+    bl_idname = "NODE_MT_category_IMPORT"
+    bl_label = "Import"
+
+    def draw(self, _context):
+        layout = self.layout
+        node_add_menu.add_node_type(layout, "GeometryNodeImportSTL")
+        node_add_menu.draw_assets_for_catalog(layout, "Input/Import")
 
 class NODE_MT_geometry_node_mesh_topology(Menu):
     bl_idname = "NODE_MT_geometry_node_mesh_topology"
@@ -816,6 +826,7 @@ classes = (
     NODE_MT_geometry_node_GEO_MESH_OPERATIONS,
     NODE_MT_category_GEO_UV,
     NODE_MT_category_PRIMITIVES_MESH,
+    NODE_MT_category_IMPORT,
     NODE_MT_geometry_node_mesh_topology,
     NODE_MT_category_GEO_POINT,
     NODE_MT_category_simulation,

scripts/startup/bl_ui/space_userpref.py
@@ -2798,6 +2798,7 @@ class USERPREF_PT_experimental_new_features(ExperimentalPanel, Panel):
                 ({"property": "use_extended_asset_browser"},
                  ("blender/blender/projects/10", "Pipeline, Assets & IO Project Page")),
                 ({"property": "use_new_volume_nodes"}, ("blender/blender/issues/103248", "#103248")),
+                ({"property": "use_new_file_import_nodes"}, ("blender/blender/issues/122846", "#122846")),
                 ({"property": "use_shader_node_previews"}, ("blender/blender/issues/110353", "#110353")),
             ),
         )

source/blender/blenkernel/BKE_node.hh
@@ -1333,6 +1333,7 @@ void BKE_nodetree_remove_layer_n(bNodeTree *ntree, Scene *scene, int layer_index
 #define GEO_NODE_TOOL_ACTIVE_ELEMENT 2135
 #define GEO_NODE_SET_INSTANCE_TRANSFORM 2136
 #define GEO_NODE_INPUT_INSTANCE_TRANSFORM 2137
+#define GEO_NODE_IMPORT_STL 2138
 
 /** \} */
 

source/blender/io/stl/IO_stl.cc
@@ -23,3 +23,8 @@ void STL_export(bContext *C, const STLExportParams *export_params)
   SCOPED_TIMER("STL Export");
   blender::io::stl::exporter_main(C, *export_params);
 }
+
+Mesh *STL_import_mesh(const STLImportParams *import_params)
+{
+  return blender::io::stl::read_stl_file(*import_params);
+}

source/blender/io/stl/IO_stl.hh
@@ -14,6 +14,7 @@
 
 #include "IO_orientation.hh"
 
+struct Mesh;
 struct bContext;
 struct ReportList;
 
@@ -48,3 +49,5 @@ struct STLExportParams {
 
 void STL_import(bContext *C, const STLImportParams *import_params);
 void STL_export(bContext *C, const STLExportParams *export_params);
+
+Mesh *STL_import_mesh(const STLImportParams *import_params);

source/blender/io/stl/importer/stl_import.hh
@@ -12,12 +12,16 @@
 
 struct bContext;
 struct Main;
+struct Mesh;
 struct Scene;
 struct ViewLayer;
 
 namespace blender::io::stl {
 
 void stl_import_report_error(FILE *file);
+
+/* Used from Geo nodes import for Mesh* access */
+Mesh *read_stl_file(const STLImportParams &import_params);
 
 /* Main import function used from within Blender. */
 void importer_main(const bContext *C, const STLImportParams &import_params);
@@ -27,5 +31,4 @@ void importer_main(Main *bmain,
                    Scene *scene,
                    ViewLayer *view_layer,
                    const STLImportParams &import_params);
-
 }  // namespace blender::io::stl

source/blender/makesdna/DNA_userdef_types.h
@@ -752,10 +752,11 @@ typedef struct UserDef_Experimental {
   char use_grease_pencil_version3;
   char enable_overlay_next;
   char use_new_volume_nodes;
+  char use_new_file_import_nodes;
   char use_shader_node_previews;
   char use_grease_pencil_version3_convert_on_load;
   char use_animation_baklava;
-  char _pad[3];
+  char _pad[2];
   /** `makesdna` does not allow empty structs. */
 } UserDef_Experimental;
 

zyxcambridge/blender_llm

commit 228f824b8006900e8b976805da8fafd48b57a7fb

Fix: STL import crash when unable to open or read file
Regression from d1455c41389302eadeeae1d291b482c1fa499a9f

Ensure we return from `importer_main` if no mesh could be loaded with
`read_stl_file` (e.g. `bpy.ops.wm.stl_import(filepath="<nope>")`)

Pull Request: https://projects.blender.org/blender/blender/pulls/129759

source/blender/io/stl/importer/stl_import.cc
@@ -120,6 +120,9 @@ void importer_main(Main *bmain,
   BLI_path_extension_strip(ob_name);
 
   Mesh *mesh = read_stl_file(import_params);
+  if (!mesh) {
+    return;
+  }
 
   Mesh *mesh_in_main = BKE_mesh_add(bmain, ob_name);
   BKE_mesh_nomain_to_mesh(mesh, mesh_in_main, nullptr);