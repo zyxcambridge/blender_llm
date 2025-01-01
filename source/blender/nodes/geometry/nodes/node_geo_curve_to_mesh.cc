blender/blender

commit b3a06149bbed54c84cd7fa25a499cad9b418b8eb

Fix #127713: propagate materials in Curve to Mesh node
This adds material propagation in two places to fix the bug:
* Conversion from legacy to new curves.
* From the main curve input to the output mesh in the Curve to Mesh node.

Pull Request: https://projects.blender.org/blender/blender/pulls/131862

source/blender/nodes/geometry/nodes/node_geo_curve_to_mesh.cc
@@ -13,6 +13,8 @@
 #include "GEO_join_geometries.hh"
 #include "GEO_randomize.hh"
 
+#include "DNA_mesh_types.h"
+
 #include "node_geometry_util.hh"
 
 namespace blender::nodes::node_geo_curve_to_mesh_cc {
@@ -113,6 +115,8 @@ static void node_geo_exec(GeoNodeExecParams params)
     if (geometry_set.has_curves()) {
       const Curves &curves = *geometry_set.get_curves();
       Mesh *mesh = curve_to_mesh(curves.geometry.wrap(), profile_set, fill_caps, attribute_filter);
+      mesh->mat = static_cast<Material **>(MEM_dupallocN(curves.mat));
+      mesh->totcol = curves.totcol;
       geometry_set.replace_mesh(mesh);
     }
     if (geometry_set.has_grease_pencil()) {

source/blender/blenkernel/intern/curve_legacy_convert.cc
@@ -204,6 +204,9 @@ Curves *curve_legacy_to_curves(const Curve &curve_legacy, const ListBase &nurbs_
 
   radius_attribute.finish();
 
+  curves_id->mat = static_cast<Material **>(MEM_dupallocN(curve_legacy.mat));
+  curves_id->totcol = curve_legacy.totcol;
+
   return curves_id;
 }
 

zyxcambridge/blender_llm

commit 6f7986c77272384d8d6fdea7f9f7e943f1708049

Fix #132449: Crash converting empty curves to mesh
Caused by b3a06149bbed54c84cd7fa25a499cad9b418b8eb.

`curve_to_mesh` returns null for empty meshes.
This fix guards access to the result with a null check.

Pull Request: https://projects.blender.org/blender/blender/pulls/132453

source/blender/nodes/geometry/nodes/node_geo_curve_to_mesh.cc
@@ -115,8 +115,10 @@ static void node_geo_exec(GeoNodeExecParams params)
     if (geometry_set.has_curves()) {
       const Curves &curves = *geometry_set.get_curves();
       Mesh *mesh = curve_to_mesh(curves.geometry.wrap(), profile_set, fill_caps, attribute_filter);
-      mesh->mat = static_cast<Material **>(MEM_dupallocN(curves.mat));
-      mesh->totcol = curves.totcol;
+      if (mesh != nullptr) {
+        mesh->mat = static_cast<Material **>(MEM_dupallocN(curves.mat));
+        mesh->totcol = curves.totcol;
+      }
       geometry_set.replace_mesh(mesh);
     }
     if (geometry_set.has_grease_pencil()) {